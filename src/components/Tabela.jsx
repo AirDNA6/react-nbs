a09a2ec47b0274234a828040e19ee88c5a052e05
Keyz3erS0ze <key3erS0ze@protonmail.com>
2020-12-29 00:01:31 +0100

    export disabled

--- src/components/Tabela.jsx
+++ src/components/Tabela.jsx
@@ -121,7 +121,6 @@ function Tabela() {
         </Button>
 
         <ReactHTMLTableToExcel 
-        disabled={isLoading}
         disabled={nbsList.length === 0 ? true : false}
         className="btn btn-success"
         table="table-to-xls"
