496708e3e16430bc41215225e2b6a1bc29e887ee
Keyz3erS0ze <key3erS0ze@protonmail.com>
2021-01-01 15:33:28 +0100

    first commit

--- server/database.js
+++ server/database.js
@@ -2,10 +2,12 @@ const mysql = require("mysql");
 
 var con = mysql.createPool({
   host: "eu-cdbr-west-03.cleardb.net",
-  user: "bf3270f99b543f",
-  password: "e7bb8661",
-  database: "heroku_5c7ca100f79586a",
+  user: "b28807ec9370cf",
+  password: "53c8cb0b",
+  database: "heroku_710101e64cc14fa",
 });
-
+/*
+dsadsa
+*/
 
 module.exports = con;
